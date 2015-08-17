98218f7c8cb00d1aae1a0836ad15e550ba8fc032
Stanislav Podgorskiy <stanislav@podgorskiy.com>
2015-08-16 22:48:55 -0400

    Added platform for emscripten

--- premake4.lua
+++ premake4.lua
@@ -1,13 +1,47 @@
 #!lua
 
+
+function newplatform(plf)
+    local name = plf.name
+    local description = plf.description
+ 
+    -- Register new platform
+    premake.platforms[name] = {
+        cfgsuffix = "_"..name,
+        iscrosscompiler = true
+    }
+ 
+    -- Allow use of new platform in --platfroms
+    table.insert(premake.option.list["platform"].allowed, { name, description })
+    table.insert(premake.fields.platforms.allowed, name)
+ 
+    -- Add compiler support
+    -- gcc
+    premake.gcc.platforms[name] = plf.gcc
+    --other compilers (?)
+end
+ 
+
+newplatform {
+    name = "emscripten",
+    description = "emscripten",
+    gcc = {
+        cc = "emcc",
+        cxx = "emcc",
+        cppflags = ""
+    }
+}
+ 
 solution "EnvMapTool"
    configurations { "Debug", "Release" }
+   platforms { "native", "emscripten" }
+
    location ("projects/" .. _ACTION)
 
    include "libs/libpng"
    include "libs/zlib"
    include "libs/libjpeg"
-   include "libs/targa"
+   --include "libs/targa"
    
    project "EnvMapTool"
       kind "ConsoleApp"
